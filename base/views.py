f153295d5b116724efd045987128fac1fb9443b2
Gaurav Bhatt <gauravkapadibhatt@gmail.com>
2025-03-07 11:48:48 +0545

    COnfigured the search bar.

--- base/views.py
+++ base/views.py
@@ -2,21 +2,46 @@ from django.shortcuts import render, get_object_or_404,redirect
 from django.views import View
 from .models import Item
 from .forms import ItemForm
+from django.db.models import Q
 
 
 class home(View):
-    def get(self,request,*args,**kwargs):
-        items = Item.objects.all()
-        expired = Item.objects.values('is_expired')
-        completed = Item.objects.values('is_completed')
-        context={'items':items,'expired':expired,'completed':completed}
-        return render(request,'base/home.html',context)
+    def get(self, request, *args, **kwargs):
+        expired = Item.objects.filter(is_expired=True)
+        completed = Item.objects.filter(Q(is_expired=False) & Q(is_completed=True))
+        incomplete = Item.objects.filter(Q(is_expired=False) & Q(is_completed=False))
+        context = {'expired': expired, 'completed': completed, 'incomplete': incomplete}
+        return render(request, 'base/home.html', context)
+
+    def post(self, request, *args, **kwargs):
+        pk = request.POST.get('name')
+        items = Item.objects.filter(name__icontains=pk)
+
+        # Re-fetch get method data
+        expired = Item.objects.filter(is_expired=True)
+        completed = Item.objects.filter(Q(is_expired=False) & Q(is_completed=True))
+        incomplete = Item.objects.filter(Q(is_expired=False) & Q(is_completed=False))
+
+        context = {
+            'items': items,
+            'expired': expired,
+            'completed': completed,
+            'incomplete': incomplete
+        }
+        return render(request, 'base/home.html', context)
+
 
 class itemview(View):
     def get(self,request,*args,**kwargs):
         pk = kwargs.get('pk')
         item_instance = get_object_or_404(Item,pk=pk)
         return render(request,'base/item.html',{'item':item_instance})
+    def post(self,request,*args,**kwargs):
+        pk = kwargs.get('pk')
+        item = Item.objects.get(id=pk)
+        item.is_completed=True
+        item.save()
+        return redirect('home')
 
 class create(View):
     def get(self,request,*args,**kwargs):
